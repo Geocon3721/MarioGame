18a06fd2a2b91d11ab502c4f9157d6299a9f670a
Geocon3721 <geocon3721@gmail.com>
2023-11-14 15:03:28 -0800

    Added part of world 1 song

--- main.ts
+++ main.ts
@@ -1,3 +1,39 @@
+function normalMusic () {
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(784, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(330, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(440, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(494, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(466, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(440, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(880, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+}
 function _11map () {
     led.unplot(X, Y - 1)
     led.unplot(X, Y)
@@ -49,7 +85,6 @@ function _11map () {
     led.unplot(X + 8, Y - 1)
     led.unplot(X + 8, Y)
     led.unplot(X + 8, Y + 1)
-    led.plotBrightness(X + 8, Y + 2, 75)
     led.unplot(X + 8, Y + 3)
     led.plotBrightness(X + 8, Y + 4, 10)
     led.unplot(X + 9, Y - 1)
@@ -65,13 +100,15 @@ function _11map () {
     led.unplot(X + 10, Y + 3)
     led.plotBrightness(X + 10, Y + 4, 10)
 }
-let Coins = 0
 let CoinBlock11A = 0
+let Coins = 0
+let CoinBlock11B = 0
 let ScreenMoveJump = 0
 let Jump = 0
 let OffSet = 0
 let Y = 0
 let X = 0
+let Music = 0
 if (false) {
     basic.showString("Press A To Go Left")
     basic.pause(100)
@@ -87,24 +124,62 @@ if (false) {
     basic.pause(100)
     basic.showString("0 Coins")
 }
+Music = 1
 X = 0
 Y = 0
 basic.forever(function () {
     _11map()
     led.plotBrightness(OffSet + 2, Jump + (3 + ScreenMoveJump), 200)
-    if (X - OffSet == -4 && Jump == 1) {	
-    } else if (X - OffSet == -7 && Jump == 1) {
-    } else if (input.buttonIsPressed(Button.A) && OffSet > -2) {
+    if (input.buttonIsPressed(Button.A) && OffSet > -2) {
         OffSet = OffSet - 1
         basic.pause(200)
     }
-    if (X - OffSet == -2 && Jump == 1) {	
+    if (X - OffSet == -2 && Jump == 1) {
+    	
     } else if (input.buttonIsPressed(Button.B) && OffSet < 0) {
         OffSet = OffSet + 1
         basic.pause(200)
     } else if (input.buttonIsPressed(Button.B) && X > -6) {
         X = X - 1
         basic.pause(200)
+    }
+})
+basic.forever(function () {
+    if (CoinBlock11B >= 2) {
+        led.plotBrightness(X + 8, Y + 2, 10)
+    } else if (CoinBlock11B > 0) {
+        Coins = Coins + 1
+        CoinBlock11B = CoinBlock11B + 1
+    }
+})
+basic.forever(function () {
+    if (X - OffSet == -3 && Jump == -2 || X - OffSet == -6 && Jump == -2) {
+        if (input.isGesture(Gesture.Shake)) {
+            Jump = Jump - 1
+            basic.pause(200)
+            Jump = Jump - 1
+            basic.pause(200)
+        }
+    } else if (Jump < 0) {
+        basic.pause(200)
+        Jump = Jump + 1
+    } else if (input.isGesture(Gesture.Shake)) {
+        if (X - OffSet == -3) {
+            CoinBlock11A = CoinBlock11A + 1
+        } else if (X - OffSet == -6) {
+            CoinBlock11B = CoinBlock11B + 1
+        } else {
+            Jump = Jump - 1
+            basic.pause(200)
+        }
+        if (X - OffSet == -3 && Jump == 0) {
+            CoinBlock11A = CoinBlock11A + 1
+        } else if (X - OffSet == -6 && Jump == 0) {
+            CoinBlock11B = CoinBlock11B + 1
+        } else {
+            Jump = Jump - 1
+            basic.pause(500)
+        }
     }
 })
 basic.forever(function () {
@@ -143,36 +218,41 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
+    if (Music == 1) {
+        normalMusic()
+    }
+})
+basic.forever(function () {
+    if (CoinBlock11B == 0) {
+        led.unplot(X + 8, Y + 2)
+        basic.pause(100)
+        led.plotBrightness(X + 8, Y + 2, 100)
+        basic.pause(100)
+        led.plotBrightness(X + 8, Y + 2, 100)
+        basic.pause(100)
+        led.plotBrightness(X + 8, Y + 2, 100)
+        basic.pause(100)
+        led.plotBrightness(X + 8, Y + 2, 100)
+        basic.pause(100)
+        led.plotBrightness(X + 8, Y + 2, 100)
+        basic.pause(100)
+        led.plotBrightness(X + 8, Y + 2, 100)
+        basic.pause(100)
+        led.plotBrightness(X + 8, Y + 2, 100)
+        basic.pause(100)
+        led.plotBrightness(X + 8, Y + 2, 100)
+        basic.pause(100)
+        led.plotBrightness(X + 8, Y + 2, 100)
+        basic.pause(100)
+        led.plotBrightness(X + 8, Y + 2, 100)
+        basic.pause(100)
+    }
+})
+basic.forever(function () {
     if (CoinBlock11A >= 2) {
         led.plotBrightness(X + 5, Y + 2, 10)
     } else if (CoinBlock11A > 0) {
         Coins = Coins + 1
         CoinBlock11A = CoinBlock11A + 1
-    }
-})
-basic.forever(function () {
-    if (X - OffSet == -3 && Jump == -2) {
-        if (X - OffSet == -3 && (Jump == -2 && input.isGesture(Gesture.Shake))) {
-            Jump = Jump - 1
-            basic.pause(200)
-            Jump = Jump - 1
-            basic.pause(200)
-        }
-    } else if (Jump < 0) {
-        Jump = Jump + 1
-        basic.pause(200)
-    } else if (input.isGesture(Gesture.Shake)) {
-        if (X - OffSet == -3 && Jump == 0) {
-            CoinBlock11A = CoinBlock11A + 1
-        } else {
-            Jump = Jump - 1
-            basic.pause(200)
-        }
-        if (X - OffSet == -3 && Jump == 0) {
-            CoinBlock11A = CoinBlock11A + 1
-        } else {
-            Jump = Jump - 1
-            basic.pause(500)
-        }
     }
 })
