4d864bb379b885d0406aacc290babe06d40f71ba
Geocon3721 <geocon3721@gmail.com>
2023-11-21 15:01:02 -0800

    Update on 1-1 song

--- main.ts
+++ main.ts
@@ -33,6 +33,155 @@ function normalMusic () {
     music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
     music.play(music.tonePlayable(880, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
     music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(698, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(784, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(494, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(330, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(440, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(494, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(466, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(440, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(880, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(698, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(784, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(494, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(784, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(698, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(440, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(440, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(784, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(698, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(1046, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(1046, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.play(music.tonePlayable(1046, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(784, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(698, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(440, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(440, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(622, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.play(music.tonePlayable(784, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(698, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(440, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(440, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(784, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(698, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(1046, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(1046, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.play(music.tonePlayable(1046, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(784, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(698, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(440, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(440, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(622, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
 }
 function _11map () {
     led.unplot(X, Y - 1)
@@ -100,15 +249,14 @@ function _11map () {
     led.unplot(X + 10, Y + 3)
     led.plotBrightness(X + 10, Y + 4, 10)
 }
-let CoinBlock11A = 0
 let Coins = 0
-let CoinBlock11B = 0
 let ScreenMoveJump = 0
+let CoinBlock11B = 0
+let CoinBlock11A = 0
 let Jump = 0
 let OffSet = 0
 let Y = 0
 let X = 0
-let Music = 0
 if (false) {
     basic.showString("Press A To Go Left")
     basic.pause(100)
@@ -124,34 +272,9 @@ if (false) {
     basic.pause(100)
     basic.showString("0 Coins")
 }
-Music = 1
+let Music = 1
 X = 0
 Y = 0
-basic.forever(function () {
-    _11map()
-    led.plotBrightness(OffSet + 2, Jump + (3 + ScreenMoveJump), 200)
-    if (input.buttonIsPressed(Button.A) && OffSet > -2) {
-        OffSet = OffSet - 1
-        basic.pause(200)
-    }
-    if (X - OffSet == -2 && Jump == 1) {
-    	
-    } else if (input.buttonIsPressed(Button.B) && OffSet < 0) {
-        OffSet = OffSet + 1
-        basic.pause(200)
-    } else if (input.buttonIsPressed(Button.B) && X > -6) {
-        X = X - 1
-        basic.pause(200)
-    }
-})
-basic.forever(function () {
-    if (CoinBlock11B >= 2) {
-        led.plotBrightness(X + 8, Y + 2, 10)
-    } else if (CoinBlock11B > 0) {
-        Coins = Coins + 1
-        CoinBlock11B = CoinBlock11B + 1
-    }
-})
 basic.forever(function () {
     if (X - OffSet == -3 && Jump == -2 || X - OffSet == -6 && Jump == -2) {
         if (input.isGesture(Gesture.Shake)) {
@@ -254,5 +377,30 @@ basic.forever(function () {
     } else if (CoinBlock11A > 0) {
         Coins = Coins + 1
         CoinBlock11A = CoinBlock11A + 1
+    }
+})
+basic.forever(function () {
+    _11map()
+    led.plotBrightness(OffSet + 2, Jump + (3 + ScreenMoveJump), 200)
+    if (input.buttonIsPressed(Button.A) && OffSet > -2) {
+        OffSet = OffSet - 1
+        basic.pause(200)
+    }
+    if (X - OffSet == -2 && Jump == 1) {
+    	
+    } else if (input.buttonIsPressed(Button.B) && OffSet < 0) {
+        OffSet = OffSet + 1
+        basic.pause(200)
+    } else if (input.buttonIsPressed(Button.B) && X > -6) {
+        X = X - 1
+        basic.pause(200)
+    }
+})
+basic.forever(function () {
+    if (CoinBlock11B >= 2) {
+        led.plotBrightness(X + 8, Y + 2, 10)
+    } else if (CoinBlock11B > 0) {
+        Coins = Coins + 1
+        CoinBlock11B = CoinBlock11B + 1
     }
 })
