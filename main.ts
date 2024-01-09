fe8c684b51dae6289c1e3e7a12464d68fd34811b
Geocon3721 <geocon3721@gmail.com>
2024-01-09 15:06:46 -0800

    second hole and stair hole but no clip bug fixes in stairs

--- main.ts
+++ main.ts
@@ -898,6 +898,84 @@ function _11map () {
     led.unplot(X + 33, Y + 2)
     led.unplot(X + 33, Y + 3)
     led.plotBrightness(X + 33, Y + 4, 10)
+    led.unplot(X + 34, Y - 1)
+    led.unplot(X + 34, Y)
+    led.unplot(X + 34, Y + 1)
+    led.unplot(X + 34, Y + 2)
+    led.unplot(X + 34, Y + 3)
+    led.unplot(X + 34, Y + 4)
+    led.unplot(X + 35, Y - 1)
+    led.unplot(X + 35, Y)
+    led.unplot(X + 35, Y + 1)
+    led.unplot(X + 35, Y + 2)
+    led.unplot(X + 35, Y + 3)
+    led.unplot(X + 35, Y + 4)
+    led.unplot(X + 36, Y - 1)
+    led.unplot(X + 36, Y)
+    led.unplot(X + 36, Y + 1)
+    led.unplot(X + 36, Y + 2)
+    led.unplot(X + 36, Y + 3)
+    led.plotBrightness(X + 36, Y + 4, 10)
+    led.unplot(X + 37, Y - 1)
+    led.unplot(X + 37, Y)
+    led.unplot(X + 37, Y + 1)
+    led.unplot(X + 37, Y + 2)
+    led.unplot(X + 37, Y + 3)
+    led.plotBrightness(X + 37, Y + 4, 10)
+    led.unplot(X + 38, Y - 1)
+    led.unplot(X + 38, Y)
+    led.unplot(X + 38, Y + 1)
+    led.unplot(X + 38, Y + 2)
+    led.unplot(X + 38, Y + 3)
+    led.plotBrightness(X + 38, Y + 4, 10)
+    led.unplot(X + 39, Y - 1)
+    led.unplot(X + 39, Y)
+    led.unplot(X + 39, Y + 1)
+    led.unplot(X + 39, Y + 2)
+    led.plotBrightness(X + 39, Y + 3, 10)
+    led.plotBrightness(X + 39, Y + 4, 10)
+    led.unplot(X + 40, Y - 1)
+    led.unplot(X + 40, Y)
+    led.unplot(X + 40, Y + 1)
+    led.plotBrightness(X + 40, Y + 2, 10)
+    led.plotBrightness(X + 40, Y + 3, 10)
+    led.plotBrightness(X + 40, Y + 4, 10)
+    led.unplot(X + 41, Y - 1)
+    led.unplot(X + 41, Y)
+    led.unplot(X + 41, Y + 1)
+    led.unplot(X + 41, Y + 2)
+    led.unplot(X + 41, Y + 3)
+    led.unplot(X + 41, Y + 4)
+    led.unplot(X + 42, Y - 1)
+    led.unplot(X + 42, Y)
+    led.unplot(X + 42, Y + 1)
+    led.plotBrightness(X + 42, Y + 2, 10)
+    led.plotBrightness(X + 42, Y + 3, 10)
+    led.plotBrightness(X + 42, Y + 4, 10)
+    led.unplot(X + 43, Y - 1)
+    led.unplot(X + 43, Y)
+    led.unplot(X + 43, Y + 1)
+    led.unplot(X + 43, Y + 2)
+    led.plotBrightness(X + 43, Y + 3, 10)
+    led.plotBrightness(X + 43, Y + 4, 10)
+    led.unplot(X + 44, Y - 1)
+    led.unplot(X + 44, Y)
+    led.unplot(X + 44, Y + 1)
+    led.unplot(X + 44, Y + 2)
+    led.unplot(X + 44, Y + 3)
+    led.plotBrightness(X + 44, Y + 4, 10)
+    led.unplot(X + 45, Y - 1)
+    led.unplot(X + 45, Y)
+    led.unplot(X + 45, Y + 1)
+    led.unplot(X + 45, Y + 2)
+    led.unplot(X + 45, Y + 3)
+    led.plotBrightness(X + 45, Y + 4, 10)
+    led.unplot(X + 46, Y - 1)
+    led.unplot(X + 46, Y)
+    led.unplot(X + 46, Y + 1)
+    led.unplot(X + 46, Y + 2)
+    led.unplot(X + 46, Y + 3)
+    led.plotBrightness(X + 46, Y + 4, 10)
 }
 let ScreenMoveJump = 0
 let CoinBlock11C = 0
@@ -950,7 +1028,7 @@ basic.forever(function () {
                     basic.pause(200)
                 }
             }
-        } else if (X - OffSet == -3 && Jump == -2 || X - OffSet == -9 && Jump == -2 || X - OffSet == -11 && Jump == -2 || X - OffSet == -13 && Jump == -2 || X - OffSet == -20 && Jump == -2 || X - OffSet == -24 && Jump == -2) {
+        } else if (X - OffSet == -3 && Jump == -2 || X - OffSet == -9 && Jump == -2 || X - OffSet == -11 && Jump == -2 || X - OffSet == -13 && Jump == -2 || X - OffSet == -20 && Jump == -2 || X - OffSet == -24 && Jump == -2 || X - OffSet == -38 && Jump == -2 || X - OffSet == -40 && Jump == -2) {
             if (input.isGesture(Gesture.ScreenUp) || input.isGesture(Gesture.Shake)) {
                 if (X - OffSet == -11) {
                     CoinBlock11D = CoinBlock11D + 1
@@ -965,7 +1043,7 @@ basic.forever(function () {
                     basic.pause(200)
                 }
             }
-        } else if (X - OffSet == -16 && Jump == -1) {
+        } else if (X - OffSet == -16 && Jump == -1 || X - OffSet == -37 && Jump == -1 || X - OffSet == -41 && Jump == -1) {
             if (input.isGesture(Gesture.ScreenUp) || input.isGesture(Gesture.Shake)) {
                 Jump = Jump - 1
                 basic.pause(200)
@@ -973,6 +1051,9 @@ basic.forever(function () {
                 basic.pause(200)
             }
         } else if (Jump < 0) {
+            basic.pause(200)
+            Jump = Jump + 1
+        } else if (X - OffSet == -28 && Jump > -1 || X - OffSet == -32 && Jump > -1 || X - OffSet == -33 && Jump > -1 || X - OffSet == -39 && Jump > -1 || Jump > 0) {
             basic.pause(200)
             Jump = Jump + 1
         } else if (input.isGesture(Gesture.ScreenUp) || input.isGesture(Gesture.Shake)) {
@@ -1164,18 +1245,18 @@ basic.forever(function () {
 //Movement
 basic.forever(function () {
     _11map()
-    if (Jump < 2) {
+    if (Jump < 1) {
         led.plotBrightness(OffSet + 2, Jump + (3 + ScreenMoveJump), 200)
-        if (X - OffSet == -17 && Jump == 0 || X - OffSet == -21 && Jump == 0 || X - OffSet == -21 && Jump == -1 || X - OffSet == -25 && Jump == 0 || X - OffSet == -25 && Jump == -1) {
+        if (X - OffSet == -17 && Jump == 0 || X - OffSet == -21 && Jump == 0 || X - OffSet == -21 && Jump == -1 || X - OffSet == -25 && Jump == 0 || X - OffSet == -25 && Jump == -1 || X - OffSet == -39 && Jump == -1 || X - OffSet == -39 && Jump == 0 || X - OffSet == -41 && Jump == -1 || X - OffSet == -42 && Jump == 0) {
         } else if (input.buttonIsPressed(Button.A) && OffSet > -2) {
             OffSet = OffSet - 1
             basic.pause(200)
         }
-        if (X - OffSet == -15 && Jump == 0 || X - OffSet == -19 && Jump == 0 || X - OffSet == -19 && Jump == -1 || X - OffSet == -23 && Jump == 0 || X - OffSet == -23 && Jump == -1) {
+        if (X - OffSet == -15 && Jump == 0 || X - OffSet == -19 && Jump == 0 || X - OffSet == -19 && Jump == -1 || X - OffSet == -23 && Jump == 0 || X - OffSet == -23 && Jump == -1 || X - OffSet == -36 && Jump == 0 || X - OffSet == -37 && Jump == -1 || X - OffSet == -39 && Jump == -1 || X - OffSet == -39 && Jump == 0) {
         } else if (input.buttonIsPressed(Button.B) && OffSet < 0) {
             OffSet = OffSet + 1
             basic.pause(200)
-        } else if (input.buttonIsPressed(Button.B) && X > -29) {
+        } else if (input.buttonIsPressed(Button.B) && X > -42) {
             X = X - 1
             basic.pause(200)
         }
@@ -1219,12 +1300,6 @@ basic.forever(function () {
                 Jump = -2
             }
         }
-    }
-})
-basic.forever(function () {
-    if (X - OffSet == -28 && Jump > -1) {
-        basic.pause(200)
-        Jump = Jump + 1
     }
 })
 basic.forever(function () {
